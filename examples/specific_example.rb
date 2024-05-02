ba8390e6df04c3d40679cf5b7f3bfb61df7a2248
alu0101056944 <alu0101056944@ull.edu.es>
2024-05-02 10:52:58 +0100

    Save.

--- examples/specific_example.rb
+++ examples/specific_example.rb
@@ -1,7 +1,7 @@
 DSLBoneAge.new do
   radiography name: 'Antonio'
   bone :radius, length: 21
-  atlas name: :atlas_basic_example, age: 8, genre: :male
+  atlas name: :basicatlas, age: 8, genre: :male
   comparisons
   compare :all
   show
